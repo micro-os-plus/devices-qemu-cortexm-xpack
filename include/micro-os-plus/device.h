7abf8fc51298bba4867b9fd626593db445ead1c4
Liviu Ionescu <ilg@livius.net>
2026-09-06 12:53:59 +0300

    remove handlers, moved to architecture

--- include/micro-os-plus/device.h
+++ include/micro-os-plus/device.h
@@ -129,16 +129,17 @@ extern "C"
 // The __NVIC_PRIO_BITS might be 8 for QEMU.
 
 /* --------  Configuration of Core Peripherals  ----------------------------------- */
-#define __CM33_REV                0x0000U   /* Core revision r0p1 */
-#define __SAUREGION_PRESENT       0U        /* SAU regions present */
-#define __MPU_PRESENT             1U        /* MPU present */
-#define __VTOR_PRESENT            1U        /* VTOR present */
-#define __NVIC_PRIO_BITS          4U        /* Number of Bits used for Priority Levels */
-#define __Vendor_SysTickConfig    0U        /* Set to 1 if different SysTick Config is used */
-#define __FPU_PRESENT             1U        /* FPU present */
-#define __DSP_PRESENT             1U        /* DSP extension present */
+#define __CM33_REV 0x0000U /* Core revision r0p1 */
+#define __SAUREGION_PRESENT 0U /* SAU regions present */
+#define __MPU_PRESENT 1U /* MPU present */
+#define __VTOR_PRESENT 1U /* VTOR present */
+#define __NVIC_PRIO_BITS 4U /* Number of Bits used for Priority Levels */
+#define __Vendor_SysTickConfig \
+  0U /* Set to 1 if different SysTick Config is used */
+#define __FPU_PRESENT 1U /* FPU present */
+#define __DSP_PRESENT 1U /* DSP extension present */
 
-#include "core_cm33.h"                      /* Processor and core peripherals */
+#include "core_cm33.h" /* Processor and core peripherals */
 
 #else
 #error "Device not supported"
